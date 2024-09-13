eae5766a4f4288d73cafac7fa6a7f531ab585b62
rkoji <wl0wn1212@gmail.com>
2024-09-13 21:56:41 +0900

    [Refactor] 로그 추가

--- user-service/src/main/java/com/team11/user_service/appication/service/CustomUserDetailsService.java
+++ user-service/src/main/java/com/team11/user_service/appication/service/CustomUserDetailsService.java
@@ -4,11 +4,13 @@ import com.team11.user_service.domain.model.User;
 import com.team11.user_service.domain.repository.UserRepository;
 import com.team11.user_service.presentation.request.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
@@ -19,7 +21,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
-
+        log.info("User found: " + user.getUsername());
         return new CustomUserDetails(user);
     }
 }
