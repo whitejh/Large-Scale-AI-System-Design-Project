de85fd4f3f63c7ef3658f7a8d6f283dcb5ad3161
rkoji <wl0wn1212@gmail.com>
2024-09-13 23:24:30 +0900

    [Refactor] swagger 추가

--- user-service/src/main/java/com/team11/user_service/presentation/controller/UserController.java
+++ user-service/src/main/java/com/team11/user_service/presentation/controller/UserController.java
@@ -9,6 +9,7 @@ import com.team11.user_service.presentation.request.CustomUserDetails;
 import com.team11.user_service.presentation.request.LoginRequestDto;
 import com.team11.user_service.presentation.request.SignUpRequestDto;
 import com.team11.user_service.presentation.request.UpdateUserRequestDto;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -38,13 +39,13 @@ public class UserController {
     private final RedisService redisService;
     private final JWTUtil jwtUtil;
 
-    // 회원 가입
+    @Operation(summary = "회원 가입",description = "사용자가 회원 가입을 합니다.")
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequestDto requestDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.signUp(requestDto));
     }
 
-    // 로그인
+    @Operation(summary = "로그인", description = "회원가입한 사용자가 로그인을 합니다.")
     @PostMapping("/login")
     public String login(@RequestBody LoginRequestDto requestDto) {
         // 인증 서버 로그인 시, 토큰 발급 처리
@@ -75,46 +76,46 @@ public class UserController {
     }
 
     // 권한 확인
-    @GetMapping("/manager")
-    @PreAuthorize("hasRole('MANAGER')")  // 'ADMIN' 역할을 가진 사용자만 접근 가능
-    public String getAdminData(HttpServletRequest request) {
-        String username = request.getHeader("X-User-Name");
-        String rolesHeader = request.getHeader("X-User-Roles");
-        System.out.println("username = " + username);
-        System.out.println("rolesHeader = " + rolesHeader);
-        // 이 메서드는 'ADMIN' 역할을 가진 사용자만 접근할 수 있습니다.
-        return "Admin data";
-    }
+//    @GetMapping("/manager")
+//    @PreAuthorize("hasRole('MANAGER')")  // 'ADMIN' 역할을 가진 사용자만 접근 가능
+//    public String getAdminData(HttpServletRequest request) {
+//        String username = request.getHeader("X-User-Name");
+//        String rolesHeader = request.getHeader("X-User-Roles");
+//        System.out.println("username = " + username);
+//        System.out.println("rolesHeader = " + rolesHeader);
+//        // 이 메서드는 'ADMIN' 역할을 가진 사용자만 접근할 수 있습니다.
+//        return "Admin data";
+//    }
 
-    // 회원 정보 수정
+    @Operation(summary = "회원 정보 수정",description = "MANAGER,DRIVER,COMPANY 권한을 가진 회원만 회원 정보 수정이 가능합니다.")
     @PutMapping()
     @PreAuthorize("hasRole('MANANGER') or hasRole('DRIVER') or hasRole('COMPANY')")
     public ResponseEntity<MessageResponseDto> updateUser(@RequestBody UpdateUserRequestDto requestDto) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(requestDto));
     }
 
-    // 회원 탈퇴
+    @Operation(summary = "회원 탈퇴",description = "사용자가 회원 탈퇴를 합니다.")
     @DeleteMapping("/{userId}")
     @PreAuthorize("hasRole('MASTER') or hasRole('MANAGER') or hasRole('DRIVER') or hasRole('COMPANY') ")
     public ResponseEntity<MessageResponseDto> deleteUser(@PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.deleteUser(userId));
     }
 
-    // 슬랙Id 확인
+    @Operation(summary = "슬랙Id 확인", description = "사용자의 슬랙Id를 확인합니다.")
     @GetMapping("/divers/{userId}")
     @PreAuthorize("hasRole('MASTER') or hasRole('MANAGER')")
     public UUID getSlackId(@PathVariable Long userId) {
         return userService.getSlackId(userId);
     }
 
-    // 사용자 정보 조회
+    @Operation(summary = "사용자 정보 조회",description = "사용자의 정보를 조회합니다.")
     @GetMapping("/{userId}")
     @PreAuthorize("hasRole('MASTER')")
     public ResponseEntity<ResponseUserInfo> getUserInfo(@PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserInfo(userId));
     }
 
-    // 사용자 정보 전체 조회
+    @Operation(summary = "사용자 정보 전체 조회", description = "사용자의 정보를 전체 조회 합니다.")
     @GetMapping
     @PreAuthorize("hasRole('MASTER')")
     public ResponseEntity<List<ResponseUserInfo>> getAllUserInfo() {
